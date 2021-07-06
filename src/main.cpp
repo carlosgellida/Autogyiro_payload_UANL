9a84d5e182c25eab21f0ac708f4d606614d44120
gellidao <gellidao@gmail.com>
2021-07-06 13:30:49 -0500

    Radio inicialization was separated into a header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-
 #include <Arduino.h>
-#include <SPI.h>
 #include "printf.h"
-#include "RF24.h"
+#include <MPU_personalized_functions.h>
+#include <Radio_personalized_functions.h>
 
 String Arr2Str(float Arr1[5]) {
   // This function transform an 5 elements array 
@@ -17,81 +16,36 @@ String Arr2Str(float Arr1[5]) {
   return Arr_transform; 
 }
 
-// instantiate an object for the nRF24L01 transceiver
-RF24 radio(14, 12); // using pin 14 for the CE pin, and pin 12 for the CSN pin
-
-// Let these addresses be used for the pair
-uint8_t address[][6] = {"1Node", "2Node"};
-// It is very helpful to think of an address as a path instead of as
-// an identifying device destination
-
-// to use different addresses on a pair of radios, we need a variable to
-// uniquely identify which address this radio will use to transmit
-bool radioNumber = 1; // 0 uses address[0] to transmit, 1 uses address[1] to transmit
-
-// Used to control whether this node is sending or receiving
-bool role = false;  // true = TX role, false = RX role
-
-// For this example, we'll be using a payload containing
-// a single float number that will be incremented
-// on every successful transmission
-
-float payload[5] ; 
-
 void setup() {
 
   Serial.begin(115200);
-  while (!Serial) {
+  /* while (!Serial) {
     // some boards need to wait to ensure access to serial over USB
-  }
+  }*/
 
-  // initialize the transceiver on the SPI bus
-  if (!radio.begin()) {
-    Serial.println(F("radio hardware is not responding!!"));
-    while (1) {} // hold in infinite loop
-  }
-
-  // print example's introductory prompt
-  Serial.println(F("RF24/examples/GettingStarted"));
-
-  radioNumber = true ;
-  Serial.print(F("radioNumber = "));
-  Serial.println((int)radioNumber);
-
-  radio.setPALevel(RF24_PA_MAX) ; 
-
-  // save on transmission time by setting the radio to only transmit the
-  // number of bytes we need to transmit a float
-  radio.setPayloadSize(sizeof(payload)); // float datatype occupies 4 bytes
-
-  // set the TX address of the RX node into the TX pipe
-  radio.openWritingPipe(address[radioNumber]);     // always uses pipe 0
-
-  // set the RX address of the TX node into a RX pipe
-  radio.openReadingPipe(1, address[!radioNumber]); // using pipe 1
-
-  // additional setup specific to the node's role
-  if (role) {
-    radio.stopListening();  // put radio in TX mode
-  } else {
-    radio.startListening(); // put radio in RX mode
-  }
-} // setup
+  initializeRadio();
+} 
 
 void loop() {
 
-    // This device is a RX node
+  
+    // This device is a TX node
 
-    uint8_t pipe;
-    if (radio.available(&pipe)) {             // is there a payload? get the pipe number that recieved it
-      uint8_t bytes = radio.getPayloadSize(); // get the size of the payload
-      radio.read(&payload, bytes);            // fetch payload from FIFO
-      Serial.print(F("Received "));
-      Serial.print(bytes);                    // print the size of the payload
-      Serial.print(F(" bytes on pipe "));
-      Serial.print(pipe);                     // print the pipe number
-      Serial.print(F(": "));
-      Serial.println(Arr2Str(payload));                // print the payload's value
+    unsigned long start_timer = micros();                    // start the timer
+    payload_str = Arr2Str(payload) ; 
+    bool report = radio.write(&payload, sizeof(payload));      // transmit & save the report
+    unsigned long end_timer = micros();                      // end the timer
+
+    if (report) {
+      Serial.print(F("Transmission successful! "));          // payload was delivered
+      Serial.print(F("Time to transmit = "));
+      Serial.print(end_timer - start_timer);                 // print the timer result
+      Serial.print(F(" us. Sent: "));
+      Serial.println(Arr2Str(payload));                                   
+    } else {
+      Serial.println(F("Transmission failed or timed out")); // payload was not delivered
     }
 
-} // loop
+    delay(1000);  // slow transmissions down by 1 second
+
+} 
