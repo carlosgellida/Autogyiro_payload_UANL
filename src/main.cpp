21293217e6d2e97eaaffeb37b78290704ac8eae7
gellidao <gellida@gmail.com>
2021-07-21 21:35:20 -0500

    motion is computed and sended, but the library need some corrections

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,11 @@
 #include <Wire.h>
 #include <ESP32Servo.h>
 
-#include <SPI.h>  
+#include <SPI.h> 
+//#include <Quaternion&VectorAlgebra.h> 
 #include "RF24.h"
 #include <MPU_personalized_functions.h>
-#include <Quaternion&VectorAlgebra.h>
+
 
 
 
@@ -30,34 +31,21 @@ unsigned long txIntervalMillis = 10;
 Quaternion desiredQuat ; 
 Quaternion currentQuat ; 
 
+Matrix<4, 1> currentQuatArr ; 
+
 void send(void){
   bool sended = false; 
-  float time = float(micros());
-  float Quaternion[4] ;
-
-  if (mpu.update()){
-    currentQuat = get_Quaternion() ; 
-    /*Quaternion[0] = mpu.getQuaternionW() ; 
-    Quaternion[1] = mpu.getQuaternionX() ; 
-    Quaternion[2] = mpu.getQuaternionY() ; 
-    Quaternion[3] = mpu.getQuaternionZ() ; */
-    /*Serial.print("currentQuat: "); 
-    Serial.print(" "); 
-    printQuat(currentQuat); */
-  }
+  
+  currentQuat = arr2Struc(getQuatArr(mpu6050)) ; 
 
   myRadio.stopListening(); 
   sended = myRadio.write(&currentQuat, sizeof(currentQuat));
-
-  /*if(sended){
-    Serial.print("time for transmision: "); 
-    Serial.println(float(micros()) - time); 
-    //Serial.print("currentQuat: "); 
-    //printQuat(currentQuat);
-  }*/
-
   myRadio.startListening(); 
-}
+
+  if(sended){
+    printQuat(currentQuat); 
+  }
+} 
 
 bool getData(void){
   bool recieved = false; 
@@ -73,8 +61,8 @@ void showData(bool recieved){
   // If the info from joystick was recieved this 
   // functions shows the desired quaternion
   if(recieved){
-    //Serial.print("Desired quaternion (qw qx qy qz): "); 
-    //printQuat(desiredQuat); 
+    Serial.print("Desired quaternion (qw qx qy qz): "); 
+    printQuat(desiredQuat); 
   }
 }
 
@@ -108,23 +96,25 @@ void setup() {
   myRadio.openReadingPipe(1, masterAddress);
   //myRadio.setChannel(115); 	
   myRadio.setRetries(3,5);
+  myRadio.powerUp() ; 
   myRadio.startListening();
 
   delay(500) ; 
   Wire.begin(); // Iniciar I2C
   
-  inicializeMPU9250(mpu) ; // Inicializar el procesador de movimiento*/
-  
+  //inicializeMPU9250(mpu) ; // Inicializar el procesador de movimiento*/
+  inicializeMPU6050(mpu6050) ; 
+
   prevMillis = millis();
   send(); 
 }
 
 void loop() {
 
-  /* currentMillis = millis();
+  currentMillis = millis();
 
   if ((currentMillis - prevMillis) > 5 && (currentMillis - prevMillis < 6)) {
-    mpu.update();
+    mpu6050.update();
   } 
 
   if (currentMillis - prevMillis >= txIntervalMillis) {
@@ -134,41 +124,7 @@ void loop() {
 
   bool recieved = getData();
 
-  showData(recieved) ; */
+  showData(recieved) ; 
 
-  Matrix<3, 3> a1 = {1, 1, 1, 2, 4, -1, 1, -1, 2}; 
-
-  int res; 
-  Matrix<3, 3> a1_inv = a1.Inverse(&res) ; 
-  Serial << "res: " << res << "\n"; 
-
-  Serial << "a1: " << a1 << "\n" ;
-  Serial << "a1_inv: " << a1_inv << "\n";
-
-  Serial << "a1 * a1_inv: " << a1 * a1_inv << "\n"; 
-
-  Serial << "a upright corner: " << a1.Submatrix(Slice<0, 2>(), Slice<1, 3>()) << "\n" ;
-
-  Matrix<3, 2> vecI = {1, 1, 1, 2, 2, 2}; 
-  /*Matrix<3, 1> vecII = {2, 3, 4}; 
-  Matrix<1, 3, Array<1, 3, float>>  vecIII = {3, 3, 3} ; 
-
-  Serial << "crossProduct: " << crossProduct(vecI, vecII) << "\n"; 
-
-  Serial << "skewSim: " << skewSim33(vecII) << "\n"; */
-
-  Matrix<2,2> A;
-  A.Fill(1);
-
-  Matrix<2,1> B;
-  B.Fill(2);
-
-  Matrix<2,1> C = A * B;
-
-  Serial << "Matrix times vector" << C << "\n" ;
-
-  while(true){
-    //keep doing nothing
-  } 
 
 }
